0245dfe1a0a7089832ff6a611a5a20eccc11c492
Rafael Maghari <rafmaghari@Rafaels-MacBook-Pro.local>
2021-04-04 22:23:42 +0800

    add travis yml

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Tamang change lang/i);
   expect(linkElement).toBeInTheDocument();
 });
+
