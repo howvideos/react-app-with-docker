95db2337f458cad17629dddbb7aef09f33eaee4c
Rafael Maghari <rafmaghari@Rafaels-MacBook-Pro.local>
2021-04-07 16:16:13 +0800

    change 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Tamang change lang
+          Welcome to AWS
         </p>
       </header>
     </div>
