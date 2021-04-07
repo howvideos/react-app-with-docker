c9af97a2321ee8051fd2ff97d81a33f94d686874
Rafael Maghari <rafmaghari@Rafaels-MacBook-Pro.local>
2021-04-07 16:20:30 +0800

    change 2

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Tamang change lang/i);
+  const linkElement = screen.getByText(/Welcome to AWS/i);
   expect(linkElement).toBeInTheDocument();
 });
 
