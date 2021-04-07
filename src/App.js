50be80f9bf32d182f5b6da1f2f305fd38bb4632e
Rafael Maghari <rafmaghari@Rafaels-MacBook-Pro.local>
2021-04-07 16:36:24 +0800

    change 4

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to AWS
+          Welcome to AWS 1
         </p>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Welcome to AWS/i);
+  const linkElement = screen.getByText(/Welcome to AWS 1/i);
   expect(linkElement).toBeInTheDocument();
 });
 
